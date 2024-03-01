b281ee2fa9b15d84edf7b6b3e6c2f16bd23bb6a2
Aldo Ibarra <aibarra@ciatransmaritima.com>
2024-02-29 22:50:24 -0600

    Prueba Aldo Work

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Carolines Place</h1>
+      <h1>Carolines Places</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
